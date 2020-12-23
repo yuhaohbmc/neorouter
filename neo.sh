25fddc58273dd39a331d858cf97069067cc7323b
NYOOBEO <59578156+NYOOBEO@users.noreply.github.com>
2020-12-23 22:28:55 +0800

    Update neo.sh

--- neo.sh
+++ neo.sh
@@ -9,13 +9,46 @@ apt upgrade -y
 #open ip4 forwarding, TCP BBR and prevent security issues;
 cp /etc/sysctl.conf  /etc/sysctl.conf.bak
 echo net/ipv4/ip_forward=1 >> /etc/sysctl.conf
-echo net.ipv4.conf.all.proxy_arp=1 >> /etc/sysctl.conf
-echo net.ipv4.conf.all.rp_filter=1 >> /etc/sysctl.conf
-echo net.ipv4.conf.default.rp_filter=1 >> /etc/sysctl.conf
-echo net.ipv4.tcp_syncookies=1 >> /etc/sysctl.conf
-echo net.ipv4.tcp_max_syn_backlog=2048 >> /etc/sysctl.conf
-echo net.ipv4.tcp_synack_retries=2 >> /etc/sysctl.conf
-echo net.ipv4.tcp_syn_retries=5 >> /etc/sysctl.conf
+
+# Protect from IP Spoofing  
+echo net.ipv4.conf.all.rp_filter = 1 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.rp_filter = 1 >> /etc/sysctl.conf
+
+# Ignore ICMP broadcast requests
+echo net.ipv4.icmp_echo_ignore_broadcasts = 1 >> /etc/sysctl.conf
+
+# Protect from bad icmp error messages
+echo net.ipv4.icmp_ignore_bogus_error_responses = 1 >> /etc/sysctl.conf
+
+# Disable source packet routing
+echo net.ipv4.conf.all.accept_source_route = 0 >> /etc/sysctl.conf
+echo net.ipv6.conf.all.accept_source_route = 0 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.accept_source_route = 0 >> /etc/sysctl.conf
+
+
+# Turn on exec shield
+echo kernel.exec-shield = 1 >> /etc/sysctl.conf
+echo kernel.randomize_va_space = 1 >> /etc/sysctl.conf
+
+# Block SYN attacks
+echo net.ipv4.tcp_syncookies = 1 >> /etc/sysctl.conf
+echo net.ipv4.tcp_max_syn_backlog = 2048 >> /etc/sysctl.conf
+echo net.ipv4.tcp_synack_retries = 2 >> /etc/sysctl.conf
+echo net.ipv4.tcp_syn_retries = 5 >> /etc/sysctl.conf
+
+# Log Martians  
+echo net.ipv4.conf.all.log_martians = 1 >> /etc/sysctl.conf
+echo net.ipv4.icmp_ignore_bogus_error_responses = 1 >> /etc/sysctl.conf
+
+# Ignore send redirects
+echo net.ipv4.conf.all.send_redirects = 0 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.send_redirects = 0 >> /etc/sysctl.conf
+
+# Ignore ICMP redirects
+echo net.ipv4.conf.all.accept_redirects = 0 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.accept_redirects = 0 >> /etc/sysctl.conf
+echo net.ipv4.conf.all.secure_redirects = 0 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.secure_redirects = 0 >> /etc/sysctl.conf
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 sysctl -p
