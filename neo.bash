0fe5fafb6d58b659b60668a8cf9ac71dfbc4cd50
NYOOBEO <59578156+NYOOBEO@users.noreply.github.com>
2020-12-23 19:00:14 +0800

    Update neo.bash

--- neo.bash
+++ neo.bash
@@ -3,15 +3,19 @@ sudo -s
 apt update && apt upgrade -y
 apt install samba ocserv -y
 cp /etc/sysctl.conf  /etc/sysctl.conf.bak
-echo "net/ipv4/ip_forward=1" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.proxy_arp=1" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.rp_filter=1" >> /etc/sysctl.conf
-echo "net.ipv4.conf.default.rp_filter=1" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_syncookies=1" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_max_syn_backlog=2048" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_synack_retries=2" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_syn_retries=5" >> /etc/sysctl.conf
+echo net/ipv4/ip_forward=1 >> /etc/sysctl.conf
+echo net.ipv4.conf.all.proxy_arp=1 >> /etc/sysctl.conf
+echo net.ipv4.conf.all.rp_filter=1 >> /etc/sysctl.conf
+echo net.ipv4.conf.default.rp_filter=1 >> /etc/sysctl.conf
+echo net.ipv4.tcp_syncookies=1 >> /etc/sysctl.conf
+echo net.ipv4.tcp_max_syn_backlog=2048 >> /etc/sysctl.conf
+echo net.ipv4.tcp_synack_retries=2 >> /etc/sysctl.conf
+echo net.ipv4.tcp_syn_retries=5 >> /etc/sysctl.conf
+echo net.core.default_qdisc=fq >> /etc/sysctl.conf
+echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+lsmod | grep bbr
 cp /etc/ufw/before.rules   /etc/ufw/before.rules.bak
 rm /etc/ufw/before.rules
 echo -e "*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING -s 192.168.1.0/24 -o enp3s0 -j MASQUERADE\nCOMMIT" >> /etc/ufw/before.rules
