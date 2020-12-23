a9868d6f739ca5eb0d09f0f143f380a8c18060d9
NYOOBEO <59578156+NYOOBEO@users.noreply.github.com>
2020-12-23 19:22:28 +0800

    Update neo.bash

--- neo.bash
+++ neo.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Get root permission so don't need to use sudo each time 
 sudo -s
 
+# Check and install software updates
 apt update && apt upgrade -y
 
+#install software
+#Samba for file sharing
+#ocserv for VPN
+#Qbittorrent-nox-enhanced for torrent and magnet download
+#emby for internal media play
+#AdGuardHome for DNS and DHCP server
 apt install samba ocserv -y
 
+#open ip4 forwarding, TCP BBR and prevent security issues
 cp /etc/sysctl.conf  /etc/sysctl.conf.bak
 echo net/ipv4/ip_forward=1 >> /etc/sysctl.conf
 echo net.ipv4.conf.all.proxy_arp=1 >> /etc/sysctl.conf
@@ -21,12 +30,13 @@ sysctl -p
 sysctl net.ipv4.tcp_available_congestion_control
 lsmod | grep bbr
 
-
+#Add NAT to ufw
 cp /etc/ufw/before.rules   /etc/ufw/before.rules.bak
 echo -e "*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING -s 192.168.1.0/24 -o enp3s0 -j MASQUERADE\nCOMMIT" >> /etc/ufw/before.rules.new
 cat /etc/ufw/before.rules >> /etc/ufw/before.rules.new
 mv /etc/ufw/before.rules.new /etc/ufw/before.rules
 
+#ufw rules for applications and network routing
 ufw route allow in on 192.168.1.0/24 out on enp3s0
 ufw allow in on enp3s0 to any port 4443 proto tcp
 ufw allow in on enp3s0 to any port 4443 proto udp
@@ -44,6 +54,8 @@ ufw allow from 192.168.1.0/24 to any port 8096 proto tcp
 ufw allow from 192.168.1.0/24 to any port 8080 proto tcp
 ufw allow from 192.168.1.0/24 to any port 6881 proto udp
 ufw allow from 192.168.1.0/24 to any port 6881 proto tcp
+
+#restart ufw
 ufw disable
 ufw enable
 
